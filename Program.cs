b759bfa52cb378fc19dc034350280190d8f9da43
RigobertoPO <rperez@cobach.edu.mx>
2017-10-16 12:02:27 -0500

    VERSION 0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CURSO POO C#, SESIÓN 2");
+        }
+    }
+}
